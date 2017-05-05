ce8075cb54c27dd08ed7976fece4756305929428
Satoshi Komatsu <zlia.6.lj.425@gmsil.com>
2017-05-06 08:39:23 +0900

    最初のコミット

---
+++ Assets/Scripts/parameter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class parameter : MonoBehaviour {
+	public static int hp = 100;
+	public static int mp = 50;
+	public Text gage;
+	public static int sp = 15;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gage.text = "とし\nHP:" + hp.ToString() + "\nMP:" + mp.ToString()
+		+ "\nLv.23";
+	}
+}
