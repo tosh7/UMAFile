df916d6ce9856b2274431928b3ecbd7c3b52c783
Satoshi Komatsu <zlia.6.lj.425@gmsil.com>
2017-05-19 03:05:27 +0900

    完成版Ver.1.2

---
+++ Assets/Scripts/Opning.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Opning : MonoBehaviour {
+
+	public void StartButton(){
+		SceneManager.LoadScene ("dungeon1");
+	}
+}
